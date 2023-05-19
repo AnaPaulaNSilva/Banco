2d5e3208c5fa440963e238b58483e644b38f3ac4
AnaPaulaNSilva <130613321+AnaPaulaNSilva@users.noreply.github.com>
2023-05-18 22:15:35 -0300

    Atividade de banco

---
+++ App.java
@@ -0,0 +1,22 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		Conta conta = new Conta("00123-4");
+		ContaPoupanca poupanca = new ContaPoupanca("00321-4");
+		
+		
+		try {
+			conta.depositar(100);
+			conta.sacar(20);
+			conta.transferencia(poupanca, -1000);
+			poupanca.depositar(200);
+			poupanca.sacar(20);
+		} 
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+}
